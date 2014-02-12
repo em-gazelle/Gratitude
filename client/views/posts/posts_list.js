1489be82896b613ead612972d08722d901267bcc
em-gazelle <emmaf9@gmail.com>
2014-02-12 12:09:50 -0800

    Post List Controllers

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,12 +1,15 @@
 Template.postsList.helpers({
-  posts: function() {
-    //sort by post submit date with "Mongo's sort operator". In creating posts chapter, sorting posts
-    return Posts.find({}, {sort: {submitted: -1}});
-  },
+	posts: function() {
+	    //sort by post submit date with "Mongo's sort operator". In creating posts chapter, sorting posts
+	//    return Posts.find({}, {sort: {submitted: -1}});
+
+		//PostListControllers!!! yay, but currently, you have to manually refresh
+		return Posts.find({}, {sort: this.sort});	
+	},
 
 //Template.postsList.helpers({
-  hasMorePosts: function() {
-    this.posts.rewind();
-    return Router.current().limit() == this.posts.fetch().length;
-  }
+	hasMorePosts: function() {
+		this.posts.rewind();
+		return Router.current().limit() == this.posts.fetch().length;
+	}
 });
